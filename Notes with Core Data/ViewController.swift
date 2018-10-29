d02dc07bdde4e7abd78ecc47197519e4a631f2dc
Riley Evans <rllyy97@gmail.com>
2018-10-29 13:46:54 -0500

    Initial Commit

---
+++ Notes with Core Data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes with Core Data
+//
+//  Created by Riley Evans on 10/29/18.
+//  Copyright © 2018 Riley Evans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
